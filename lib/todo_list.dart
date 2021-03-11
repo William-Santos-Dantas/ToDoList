83d660abeb0fca1eb6f823d8b9860a83312fe5d9
unknown <NidrahDev@protonmail.com>
2021-03-11 10:43:52 -0300

    initial display

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -11,6 +11,17 @@ class _ToDoListState extends State<ToDoList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'All Todos',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.add),
+      ),
       body: bodyPage(),
     );
   }
@@ -18,14 +29,44 @@ class _ToDoListState extends State<ToDoList> {
   Widget bodyPage() {
     return SafeArea(
       child: Padding(
-        padding: EdgeInsets.all(25),
+        padding: EdgeInsets.all(10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'All Todos',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return listTile();
+              },
             )
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget listTile() {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: GestureDetector(
+        onTap: () {
+          print('Teste');
+        },
+        child: ListTile(
+          //value: true,
+          title: Text('One-line with both widgets'),
+          leading: CircleAvatar(
+            child: Icon(true ? Icons.check : Icons.error),
+          ),
         ),
       ),
     );
